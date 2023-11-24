e0fa972d27058ecf52ad7dbfad48d31258f42f45
i3rotlher <maxe.kw@googlemail.com>
2023-11-24 16:28:19 +0100

    tests added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,50 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Mock-Komponenten für die verschiedenen Seiten
+jest.mock('./components/LoginPage/LoginPage', () => () => <div>LoginPage</div>);
+jest.mock('./components/RegisterPage/RegisterPage', () => () => <div>RegisterPage</div>);
+jest.mock('./components/HomePage/HomePage', () => () => <div>HomePage</div>);
+jest.mock('./components/BoxDetailPage/BoxDetailPage', () => () => <div>BoxDetailPage</div>);
+jest.mock('./components/BoxAddPage/BoxAddPage', () => () => <div>BoxAddPage</div>);
+jest.mock('./components/LearnPage/LearnPage', () => () => <div>LearnPage</div>);
+jest.mock('./components/CreateCardPage/CreateCardPage', () => () => <div>CreateCardPage</div>);
+
+describe('App Routing', () => {
+  it('zeigt die HomePage auf der Hauptseite', () => {
+    render(<App />);
+    expect(screen.getByText('HomePage')).toBeInTheDocument();
+  });
+
+  it('zeigt die LoginPage auf dem /login Pfad', () => {
+    render(<App />, { route: '/login' });
+    expect(screen.getByText('LoginPage')).toBeInTheDocument();
+  });
+
+  it('zeigt die RegisterPage auf dem /register Pfad', () => {
+    render(<App />, { route: '/register' });
+    expect(screen.getByText('RegisterPage')).toBeInTheDocument();
+  });
+
+  // Ähnliche Tests für die anderen Routen
+  it('zeigt die BoxDetailPage auf dem /box/:boxId Pfad', () => {
+    render(<App />, { route: '/box/123' });
+    expect(screen.getByText('BoxDetailPage')).toBeInTheDocument();
+  });
+
+  it('zeigt die BoxAddPage auf dem /add-box Pfad', () => {
+    render(<App />, { route: '/add-box' });
+    expect(screen.getByText('BoxAddPage')).toBeInTheDocument();
+  });
+
+  it('zeigt die LearnPage auf dem /learn/:boxId Pfad', () => {
+    render(<App />, { route: '/learn/123' });
+    expect(screen.getByText('LearnPage')).toBeInTheDocument();
+  });
+
+  it('zeigt die CreateCardPage auf dem /create-card/:boxId Pfad', () => {
+    render(<App />, { route: '/create-card/123' });
+    expect(screen.getByText('CreateCardPage')).toBeInTheDocument();
+  });
 });
--- src/components/BoxAddPage/BoxAddPage.test.js
+++ src/components/BoxAddPage/BoxAddPage.test.js
@@ -1,43 +1,34 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import BoxAddPage from './BoxAddPage';
-
-// Mock des useNavigate Hooks von react-router-dom
-const mockedNavigate = jest.fn();
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: () => mockedNavigate,
-}));
+import { BrowserRouter } from 'react-router-dom';
 
 describe('BoxAddPage', () => {
-  test('renders BoxAddPage component', () => {
-    render(<BoxAddPage />);
-    
-    expect(screen.getByPlaceholderText(/boxname/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText(/beschreibung/i)).toBeInTheDocument();
+  const mockNavigate = jest.fn();
+  jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate,
+  }));
+
+  beforeEach(() => {
+    render(<BoxAddPage />, { wrapper: BrowserRouter });
+  });
+
+  it('sollte das Formular mit zwei Eingabefeldern und einem Button rendern', () => {
+    expect(screen.getByPlaceholderText('Boxname')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('Beschreibung')).toBeInTheDocument();
     expect(screen.getByRole('button', { name: /erstellen/i })).toBeInTheDocument();
   });
 
-  test('allows user to enter box name and description', () => {
-    render(<BoxAddPage />);
-    
-    fireEvent.change(screen.getByPlaceholderText(/boxname/i), { target: { value: 'Meine neue Box' } });
-    fireEvent.change(screen.getByPlaceholderText(/beschreibung/i), { target: { value: 'Das ist eine Beschreibung.' } });
-    
-    expect(screen.getByPlaceholderText(/boxname/i).value).toBe('Meine neue Box');
-    expect(screen.getByPlaceholderText(/beschreibung/i).value).toBe('Das ist eine Beschreibung.');
+  it('sollte die Eingabewerte aktualisieren, wenn der Benutzer in die Felder tippt', () => {
+    fireEvent.change(screen.getByPlaceholderText('Boxname'), { target: { value: 'Neue Box' } });
+    fireEvent.change(screen.getByPlaceholderText('Beschreibung'), { target: { value: 'Beschreibung der neuen Box' } });
+    expect(screen.getByPlaceholderText('Boxname').value).toBe('Neue Box');
+    expect(screen.getByPlaceholderText('Beschreibung').value).toBe('Beschreibung der neuen Box');
   });
 
-  test('navigates user to homepage on successful box creation', () => {
-    render(<BoxAddPage />);
-    
-    // Hier müssen Sie Ihre Logik zum Simulieren der Netzwerkanfrage einfügen
-    
+  it('sollte beim Absenden des Formulars navigieren', () => {
     fireEvent.click(screen.getByRole('button', { name: /erstellen/i }));
-    
-    // Annahme hier ist, dass die Navigation nach dem Erstellen der Box erfolgt
-    expect(mockedNavigate).toHaveBeenCalledWith('/');
+    expect(mockNavigate).toHaveBeenCalled();
   });
-
-  // Weitere Tests hier hinzufügen, um spezifische Interaktionen und Logik zu prüfen
 });
